f0304a24205fd3e74cad491f500668cdc8481a1d
miaoyu <miaoyu@ifun.com>
2023-06-17 10:43:09 +0800

    feat: imports

--- global-and-lifecycle/src/bbb/bbb.service.ts
+++ global-and-lifecycle/src/bbb/bbb.service.ts
@@ -1,15 +1,18 @@
+import { AaaService } from './../aaa/aaa.service';
 import { Injectable } from '@nestjs/common';
 import { CreateBbbDto } from './dto/create-bbb.dto';
 import { UpdateBbbDto } from './dto/update-bbb.dto';
 
 @Injectable()
 export class BbbService {
+  constructor(private aaaService: AaaService) {}
   create(createBbbDto: CreateBbbDto) {
+    // this.aaaService.create
     return 'This action adds a new bbb';
   }
 
   findAll() {
-    return `This action returns all bbb`;
+    return `This action returns all bbb` + this.aaaService.findAll();
   }
 
   findOne(id: number) {
