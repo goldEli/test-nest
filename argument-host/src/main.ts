c4c4d795fd6c957e4fe82c4bf6280d1507b073f2
缪宇 <miaoyu2009@qq.com>
2023-06-20 10:29:52 +0800

    feat: executionContext

--- argument-host/src/main.ts
+++ argument-host/src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { AaaFilter } from './aaa.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalFilters(new AaaFilter());
+
   await app.listen(3000);
 }
 bootstrap();
